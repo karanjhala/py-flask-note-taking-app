bffbdd21c4ea543ac144ada349b5efde4253105b
Karan Jhala <karanjhala@gmail.com>
2020-03-29 11:35:05 +0000

    Implement Sign up, Log in, Log out Views and User Authentication

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -14,10 +15,68 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    from .models import db
+    from .models import db, User
 
     db.init_app(app)
     migrate = Migrate(app, db)
+
+    @app.route('/sign_up',methods=('GET','POST'))
+    def sign_up():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            error = None
+
+            if not username:
+                error = 'Username is required'
+            elif not password:
+                error = 'Password is required'
+            elif User.query.filter_by(username=username).first():
+                error = 'Username is already taken'
+
+            if error is None:
+                user = User(username=username, password=password)
+                db.session.add(user)
+                db.session.commit()
+                flash("Successfully Signed Up! Please log in to the App.", 'success')
+                return redirect(url_for('log_in'))
+
+            flash(error, 'error')
+
+        return render_template('sign_up.html')
+
+    @app.route('/log_in',methods=('GET','POST'))
+    def log_in():
+
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            error = None
+
+            user = User.query.filter_by(username=username).first()
+
+            if not username or not password:
+                error = 'Username or password are incorrect'
+
+            if error is None:
+                session.clear()
+                session['user.id'] = user.id
+                return redirect(url_for('index'))
+
+            flash (error, 'error')
+
+        return render_template('log_in.html')
+
+    @app.route('/log_out', methods=('GET','DELETE'))
+    def log_out():
+        session.clear()
+        flash("Successfully logged out!", 'success')
+        return redirect(url_for('log_in'))
+
+    @app.route('/')
+    def index():
+
+        return "index"
 
     return app
 
